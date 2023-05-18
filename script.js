9c00a9c51899a35a4fb6adf2b5b8f32378142acb
caglenoah <caglenoah@gmail.com>
2023-05-18 12:46:28 -0600

    added java script. set target date. added comments/notes. some from the the original writer, some notes from myself.

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+//this is the variable to manage data rules
 const date = new Date();
-date.setDate(date.getDate() + 30)
+//sets target date. (8)  represents days till event.
+date.setDate(date.getDate() + 8) 
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 
+// converts target date to a date object
 let targetDate = date;
 
-let x = setInterval(function() {
+//sets the loop to update the countdown once a second
+let x = setInterval(function () {
     
+    // gets the current time
     let now = new Date().getTime();
+
+    // gets the time remaining
     let distance = targetDate - now;
 
-    document.querySelector('#days').innerHTML = Math.floor(distance / (day)) + "days, ";
-    document.querySelector('#hours').inntHTML = Math.floor((distance % (day)) / (hour)) + "hours, ";
-    document.querySelector('#minutes').innerHTML = Math.floor((distance % (hour)) / (minute)) + " minutes, ";
-    ('#seconds').innerHTML = Math.floor((distance % (minute)) / second)+" seconds ";
-}, second)
-
+    // updates css span tags with countdown values
+    document.querySelector('#days').innerHTML = Math.floor(distance / (day))+" days, ";
+    document.querySelector('#hours').innerHTML = Math.floor((distance % (day)) / (hour))+" hours, ";
+    document.querySelector('#minutes').innerHTML = Math.floor((distance % (hour)) / (minute))+" minutes, ";
+    document.querySelector('#seconds').innerHTML = Math.floor((distance % (minute)) / second)+" seconds ";
+  }, second)
+    
+  
